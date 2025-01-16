5e0ce6766383b092436cf71fc504c3a9fd662bd4
wntjr0219 <90208937+wntjr0219@users.noreply.github.com>
2025-01-16 17:48:49 +0900

    imgui 테스트
    imgui 파일 간략화

--- imgui/examples/example_win32_directx12/main.cpp
+++ imgui/examples/example_win32_directx12/main.cpp
@@ -1,6 +1,6 @@
 #include "imgui.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx12.h"
+#include "use/imgui_impl_win32.h"
+#include "use/imgui_impl_dx12.h"
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <tchar.h>
